1a8b89c230f43a63786f2889aaa007b5ea5350ee
VaseSimion <sularea.vasile@gmail.com>
2022-07-23 08:45:53 +0200

    Made the uplaod part of the pictures

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import Config as Cfg
 
 
 def fb_test():
-    params = Cfg.params
+    params = Cfg.get_parameters("test")
     # Define url
     url = params['endpoint_base'] + params['instagram_account_id']
 
@@ -18,3 +18,24 @@ def fb_test():
     data = requests.get(url, endpoint_params)
     basic_insight = json.loads(data.content)
     print(basic_insight)
+
+    upload = dict()
+    upload['image_url'] = "https://i.postimg.cc/Hxr0mXYd/DSC04936.jpg"
+    upload['caption'] = "This just might be it!"
+    upload['access_token'] = params['access_token']
+
+    url = params['endpoint_base'] + params['instagram_account_id'] +"/media"
+
+    creation_id = requests.post(url, upload)
+    print(creation_id)
+    container_id = creation_id.json()['id']
+    url = params['endpoint_base'] + params['instagram_account_id'] +"/media_publish"
+    image_params = dict()
+    image_params['creation_id'] = str(container_id)
+    image_params['access_token'] = params['access_token']
+    response = requests.post(url, image_params)
+
+    print(response)
+
+
+
